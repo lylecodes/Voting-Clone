a62b745cfd9dfe8015d826694348393d52a7aba9
Lyle Jensen <lyle.jensen95@gmail.com>
2022-03-04 20:00:59 -0700

    added css

--- script.js
+++ script.js
@@ -1,30 +1,6 @@
-const loadVariable = (src, type) => {
-  const script = document.createElement("script");
-  script.src = src;
-  document.head.append(script);
-
-  if (type === "apiKey") return script.FIREBASE_API_KEY;
-  throw Error("Variable type is undefined");
-};
-
-const apiKey = loadVariable("variables.js", "apiKey");
-
-const firebaseConfig = {
-  apiKey: apiKey,
-  authDomain: "voting-b90b3.firebaseapp.com",
-  databaseURL: "https://voting-b90b3-default-rtdb.firebaseio.com",
-  projectId: "voting-b90b3",
-  storageBucket: "voting-b90b3.appspot.com",
-  messagingSenderId: "505804863400",
-  appId: "1:505804863400:web:ef4e6304954821df704f9d",
-  measurementId: "G-DY9N2MBC3F",
-};
-firebase.initializeApp(firebaseConfig);
-
 const dbRef = firebase.database().ref();
 
 dbRef.on("value", (snapshot) => {
-  console.log("reload");
   if (snapshot.exists()) {
     const data = snapshot.val();
     const totalVotes = getVoteTotals(data);
@@ -38,10 +14,11 @@ const loadParadePhotos = (data, totalVotes) => {
   let hasVoted = localStorage.getItem("photo vote");
 
   const oldPhotosContainer = document.querySelector("#photos_container");
+
   if (oldPhotosContainer !== null) {
-    console.log("removed");
     oldPhotosContainer.remove();
   }
+
   const photosContainer = document.createElement("div");
   photosContainer.setAttribute("id", "photos_container");
 
@@ -51,7 +28,7 @@ const loadParadePhotos = (data, totalVotes) => {
     const { name, srcUrl, altText, votes } = photoInfo;
 
     const photoContainer = document.createElement("div");
-    photoContainer.setAttribute("class", "photo");
+    photoContainer.setAttribute("class", "photo_container");
 
     const photoName = document.createElement("p");
     photoName.setAttribute("class", "photo_name");
@@ -63,18 +40,19 @@ const loadParadePhotos = (data, totalVotes) => {
     image.setAttribute("alt", altText);
 
     const voteContainer = document.createElement("div");
+    voteContainer.setAttribute("class", "vote_btn_container");
 
     const voteBtn = document.createElement("button");
     console.log(voteBtn.innerHTML);
     voteBtn.setAttribute("id", `${index}`);
     voteBtn.setAttribute("class", "btn btn-primary");
 
-    console.log(hasVoted);
     if (!hasVoted) {
       voteBtn.addEventListener("click", castVote);
       voteBtn.innerHTML = "Vote";
     } else {
       voteBtn.innerHTML = `Votes: ${votes}, Total Votes: ${totalVotes}`;
+      voteBtn.disabled = true;
     }
 
     voteContainer.appendChild(voteBtn);
@@ -102,6 +80,7 @@ const getVoteTotals = (data) => {
 const castVote = (evt) => {
   localStorage.setItem("photo vote", "voted");
   const id = evt.target.id;
+
   dbRef
     .child("paradePhotos")
     .child(id)
@@ -111,11 +90,11 @@ const castVote = (evt) => {
         const photoInfo = snapshot.val();
         updateVote(photoInfo, id);
       } else {
-        console.log("No data available");
+        throw Error("No data available");
       }
     })
     .catch((error) => {
-      console.error(error);
+      throw Error(error);
     });
 };
 
